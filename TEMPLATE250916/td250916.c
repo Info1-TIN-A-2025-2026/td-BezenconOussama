dad4d418db321efd384cd614259f23b89fb4c86f
BezenconOussama <antoine.bezencon@heig-vd.ch>
2025-09-16 12:32:46 +0000

    travail avec des variables

---
+++ TEMPLATE250916/td250916.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+	unsigned int NombreVuej = 23;//entier non signé
+	int detraquer = -88;//entier signé
+	float temperature = 234, 39393;//32bit real
+	double temperatureprecise = 23.39393939393939;//64bit reals
+}
