d4800eaef77edebc157587f8b7ecd0b96f956e60
BezenconOussama <antoine.bezencon@heig-vd.ch>
2025-09-29 13:38:28 +0000

    convertisseur sec en h:min:sec

--- TEMPLATE250916/td250916.c
+++ TEMPLATE250916/td250916.c
@@ -2,8 +2,31 @@
 
 int main(int argc, const char *argv[])
 {
-	unsigned int NombreVuej = 23;//entier non signé
-	int detraquer = -88;//entier signé
-	float temperature = 234, 39393;//32bit real
-	double temperatureprecise = 23.39393939393939;//64bit reals
+	/*On prend la valeurs de la console
+	 On la compare a 0 pour affichéé error
+	 Si positif Error
+	 On prend s
+	 On la compare a 86400 pour affiché overflow
+	 Si positif Overflow
+	 On divise par 3600 pour les heures h on divise par 60 pour les minutes m on fait modulo 60 pour le reste pour les seocnde s
+	 On concat tout dans 1 string
+	 On print dedans */
+	int s = 0;
+	printf("Number of seconds");
+	if (s < 0)
+	{
+		printf("ERROR \n");
+	}
+	else if (s > 86400)
+
+	{
+		printf("OVERFLOW \n");
+	}
+	else
+	{
+		int h = s / 3600;
+		int m = s % 3600;
+		int s2 = s % 60;
+		printf(h, ":",m, ":", s2);
+	}
 }
